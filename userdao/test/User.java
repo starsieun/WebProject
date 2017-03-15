d07c6f5a0b805cfd0bca721f6aede25bea59ddce
starsieun <risieun513@naver.com>
2017-03-15 19:45:20 +0900

    readme

---
+++ userdao/test/User.java
@@ -0,0 +1,5 @@
+/**
+ * Created by sieun on 2017. 3. 15..
+ */
+public class User {
+}
---
+++ userdao/test/UserDao.java
@@ -0,0 +1,16 @@
+/**
+ * Created by sieun on 2017. 3. 15..
+ */
+public class UserDao {
+
+    //User 어디에 있어? Mysql
+    //
+    //Class를  로딩해야되네
+    //커넥션 맺기
+    //쿼리를 만들어야겠네
+    //쿼리를 실행해야곘네
+    //실행된 결과를 객체에 매핑
+    //자원해지
+    resultSe.close();
+    //결과를 리턴
+}
